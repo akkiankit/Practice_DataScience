4ab7aeff5ad7f22f15030a4aa8af5af23e9ae616
akkiankit <akkiankitilu@gmail.com>
2019-09-25 00:58:24 +0530

    Edureka

---
+++ Edureka/First.py
@@ -0,0 +1,4 @@
+# list => [] more like array and data save can be change,Ordered, Allow duplicate => imutable
+# tuple => () Ordered and Unchangable, allow duplicate
+# set => {} : unordered and no duplicate member allowed
+# Dictionary => {} more like objects and Unordered and changable, duplicate allowable
---
+++ Edureka/List.py
@@ -0,0 +1,13 @@
+city = ["london","america","bangalore","new york"]
+print(city)
+print(city[0])
+print(city[-1])
+city.append("Bihar")
+print(city)
+city.insert(2,"Samastipur")
+print(city)
+city.pop()
+print(city.pop())
+
+#tuples
+tup = ("audi","BMW","Marc")
